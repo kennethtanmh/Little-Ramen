c74db875a96a89265c367b4a38e3004e02aea466
Yuan Sheng <ongyuansheng@yahoo.com.sg>
2023-10-31 23:01:04 +0800

    New module that handles the transfer of data to server via js rather than form submission this improves the ui by preventing the page from refreshing after everytime the user adds an item to cart

---
+++ pages/preorder/form-promise.js
@@ -0,0 +1,53 @@
+// This module allows the web page to retrive data from the server without having to reload preorder.html every time the user adds to cart
+// This module adds event listeners to all forms to handle the processing of logic after the form is submmitted
+
+const handleFormSubmit = (event) => {
+  // Prevents reloading of page after a form is submitted
+  event.preventDefault();
+
+  // Reference to the current item that is being submitted
+  const form = event.currentTarget;
+
+  // sanity check to see if the right data is passed
+  const formData = new FormData(form);
+  for (let [key, value] of formData.entries()) {
+    console.log(key, value);
+  }
+
+  // send data to the server using the Fetch API
+  sendDataToServer(formData)
+    .then((data) => {
+      // Handle response from server
+      console.log(data);
+    })
+    .catch((error) => {
+      console.error("There was an error:", error);
+    });
+};
+
+// Function to send data to the server using Fetch API
+const sendDataToServer = (formData) => {
+  return fetch("preorder.php", {
+    method: "POST",
+    body: formData,
+  }).then((response) => {
+    // Check if the response is okay, else throw an error
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    alert("item successfully added");
+  });
+};
+
+const init = () => {
+  // Select all forms with the class 'addToCartForm'
+  const forms = document.querySelectorAll(".addToCartForm");
+
+  // adds the func to every food item
+  forms.forEach((form) => {
+    form.addEventListener("submit", handleFormSubmit);
+  });
+};
+
+// Execute the initialization function once the DOM is fully loaded
+document.addEventListener("DOMContentLoaded", init);
