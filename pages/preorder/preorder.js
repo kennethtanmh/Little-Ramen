9e2ccdcc6cec82d9a89fa55d3ba698a789a6fdf2
Yuan Sheng <ongyuansheng@yahoo.com.sg>
2023-10-31 20:11:01 +0800

    added comments to preorder.js regarding the handling of item quantity

--- pages/preorder/preorder.js
+++ pages/preorder/preorder.js
@@ -1,6 +1,9 @@
+// This script handles the quantity adjustments for the items in preorder.html
 document.addEventListener("DOMContentLoaded", function () {
   const quantityValueElements = document.querySelectorAll(".quantity-value");
 
+  // Increases quantity of items
+  // Each individual items has a max quantity of 10
   document.querySelectorAll(".plus").forEach((button, index) => {
     button.addEventListener("click", () => {
       let currentValue = parseInt(quantityValueElements[index].innerText);
@@ -10,6 +13,7 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 
+  // decreases quantity of items
   document.querySelectorAll(".minus").forEach((button, index) => {
     button.addEventListener("click", () => {
       let currentValue = parseInt(quantityValueElements[index].innerText);
